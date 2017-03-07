a3b1c19a9bedbb66b496b963dbb9ef2303607ce6
Ryan Rodriguez <rjoe.rod@gmail.com>
2017-03-07 16:59:48 -0600

    Comments/Refactoring Example

--- Home/Search.js
+++ Home/Search.js
@@ -6,10 +6,14 @@ main = function(){
 	console.log(searchtime(busTime));
 }
 
-searchtime = function(param){
+// param: busTimes - an array containing each of the bus times
+//					 formatted as "pm:x:y" where 1 < x,y < 12
+searchtime = function(busTimes){
+
+	// the current time in seconds
 	var RealSecond = new Date().getHours()*3600+new Date().getMinutes()*60+new Date().getSeconds();
 loop1:
-	for(var i=0; i<param.length; i++)
+	for(var i=0; i<busTimes.length; i++)
 	{
 		var timestate = 0;
 		var BusSecond = 0;
@@ -18,21 +22,21 @@ loop1:
 		bushour = "";
 		busminutes = "";
 loop2:
-		for(var j=0; j<param[i].length; j++)
+		for(var j=0; j<busTimes[i].length; j++)
 		{
-			if(param[i][j] != ":")
+			if(busTimes[i][j] != ":")
 			{
 				if(timestate == 0)
 				{
-					noon = noon +param[i][j];
+					noon = noon +busTimes[i][j];
 				}
 				else if(timestate == 1)
 				{
-					bushour = bushour + param[i][j];
+					bushour = bushour + busTimes[i][j];
 				}
 				else
 				{
-					busminutes = busminutes + param[i][j];
+					busminutes = busminutes + busTimes[i][j];
 				}
 			}
 			else
@@ -55,20 +59,20 @@ loop2:
 		console.log("timedifference: "+timedifference);
 		if(timedifference <= 0 && RealSecond-BusSecond>=-300)
 		{
-			return("Bus is coming in "+parseInt(-timedifference/60, 10)+" minutes, run!!!!!"+" If missed, the next one comes at "+param[i+1];);
+			return("Bus is coming in "+parseInt(-timedifference/60, 10)+" minutes, run!!!!!"+" If missed, the next one comes at "+busTimes[i+1];);
 			return word;
 			break loop1;
 		}
 		else if(timedifference >= 0 && RealSecond-BusSecond<=300)
 		{
 			console.log("test2");
-			return ("You are late to the bus of "+param[i]+"! You might still be able to catch it! The next bus will come at "+param[i+1]);
+			return ("You are late to the bus of "+busTimes[i]+"! You might still be able to catch it! The next bus will come at "+busTimes[i+1]);
 			break loop1;
 		}
 		else if(timedifference < -300 && timedifference >= -1500)
 		{
 			console.log("test3");
-			return ("The next bus will come at "+ param[i]);
+			return ("The next bus will come at "+ busTimes[i]);
 			break loop1;
 		}
 	}
