70688e62a7b51a175f8d7b06521efbae3e75fe3a
Shadeoflight <joshuawu15@gmail.com>
2017-03-18 11:22:09 -0500

    Update essentialFunctions
    Adds some dynamic attributes to code.

--- PagePrototype/essentialFunctions.js
+++ PagePrototype/essentialFunctions.js
@@ -5,8 +5,37 @@ currentStatus.noBusInfo = true;
 currentStatus.route = "";
 currentStatus.stop = "";
 
+// Makes an external panel to make generating panels less code intensive.
+/*
+	Add this whenever you want to html:
+	
+	<a href="#myPanel" class="ui-btn ui-btn-inline ui-corner-all ui-shadow ui-btn-icon-left ui-icon-bars">Open Panel</a>	
+*/
+var panel = '<div data-role="panel" id="myPanel" data-position="left" data-display="reveal" data-theme="a"><h2>Menu</h2><p>Close the panel by pressing the Esc key, by swiping to the left or clicking outside of the panel.</p><div id="nextPage" data-role="controlgroup" data-type="vertical"><a href="#somePage" data-transition="slide" class="ui-btn">Button<a></div></div>';
+
+/*
+	//jQuery
+	.one() is used to attach a handler/trigger to an event for the elements. The handler/trigger is executed AT MOST ONCE per element per event type
+	
+	'pagebeforecreate' is triggered on the page being initialized, before most plugin auto-initialization occurs. Essentially, the "external-ness" of the panel.
+*/
+$(document).one('pagebeforecreate', function () {
+	$.mobile.pageContainer.prepend(panel);
+	
+	// Enhances all children of all elements in the set of matched 
+	// elements.
+	$("#myPanel").panel().enhanceWithin(); 
+});
+
+/*
+	FUNCTIONS
+*/
+
 main = function(){
-	createAndDisplayGUI();
+	
+	
+
+	//createAndDisplayGUI();
 }
 
 createAndDisplayGUI = function(){
@@ -22,8 +51,6 @@ createAndDisplayGUI = function(){
 	// create "go back" button and add it to the second page
 	$('#pageTwoButtons').append('<a href="#" class="ui-btn" data-rel="back">Go Back</a>');
 }
-
-
 
 
 
