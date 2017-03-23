4b5df268df2ecca3fae61636d7caac5e9e4de97d
rjoerod <rjoe.rod@gmail.com>
2017-03-23 12:38:18 -0500

    Fixed so map will no longer load at width/height = 0 The current fix only works when done inside the gmaps constructor. It should work within the css file, but doesn't.

--- Home/pageGeneration.js
+++ Home/pageGeneration.js
@@ -30,6 +30,7 @@ generateHomeContent = function(page) {
 	page.addContent("body",'<p class="welcome">Welcome to MyBus</p>');
 	page.addContent("body",'<p class="disclaimer">*All times are approximate. Please arrive 5 minutes early.</p>');
 	page.addContent("footer","<h1>An app developed by David Easley, Ryan Rodriguez, Josh Wu and Chen Long</h1>");
+
 }
 
 
@@ -39,26 +40,25 @@ generateMapContent = function(page) {
 	
 	page.addContent("header","<h1>MyBus City Map</h1>");
 	page.addContent("header",'<div data-role="navbar"></div>');
-	page.addContent("header",'<script type="text/javascript">' + 
-    'var map;' +
-    '$(document).ready(function(){' +
-      'map = new GMaps({' +
-        'el: "#map",' +
-        'lat: 38.955028,' + 
-        'lng: -95.262750,' +
-        'zoomControl : true,' +
-        'zoomControlOpt: {' +
-            'style : "SMALL",' +
-            'position: "TOP_LEFT"' +
-        '},' +
-        'panControl : false,' +
-        'streetViewControl : false,' +
-        'mapTypeControl: false,' +
-        'overviewMapControl: false' +
-      '});' +
-    '});' +
-  '</script>');
-  page.addContent("body", '<div class="row"> <div class="span11"> <div id="map"> </div></div></div>')
+	page.addContent("body", '<div class="row"> <div class="span11"> <div id="map"> </div></div></div>');
+	var map;
+    map = new GMaps({
+		el: "#map",
+		lat: 38.955028,  
+        lng: -95.262750, 
+        zoomControl : true, 
+        zoomControlOpt: { 
+			style : "SMALL", 
+            position: "TOP_LEFT"
+        },
+        panControl : false,
+        streetViewControl : false,
+        mapTypeControl: false,
+        overviewMapControl: false,
+		width: "95%",
+		height: "350px"
+
+    });
 
 	
 	
